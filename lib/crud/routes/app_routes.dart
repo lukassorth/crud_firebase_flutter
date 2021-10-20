735cebca0f9a2e971fa9fac956be5612c2b97968
Lucas <lukass.orth@gmail.com>
2021-10-20 12:53:12 -0300

    responsive, telas, dashboard

--- lib/crud/routes/app_routes.dart
+++
@@ -1,6 +0,0 @@
-class AppRoutes {
-  static const HOME = '/';
-  static const USER_FORM = '/user-form';
-  static const USER_LIST = '/user-list';
-  static const LOGIN = '/login';
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,35 @@
-import 'package:crud_firebase_flutter/crud/provider/users.dart';
-import 'package:crud_firebase_flutter/crud/routes/app_routes.dart';
-import 'package:crud_firebase_flutter/crud/views/home.dart';
-import 'package:crud_firebase_flutter/crud/views/login.dart';
-import 'package:crud_firebase_flutter/crud/views/user_form.dart';
-import 'package:crud_firebase_flutter/crud/views/user_list.dart';
+import 'package:crud_firebase_flutter/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+
+import 'constants.dart';
+import 'controllers/MenuController.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-// ignore: todo
-// TODO: CRUD Firebase Web cadastro de usuários.
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (contexto) => Users(),
-        )
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Crud Firebase',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          brightness: Brightness.dark,
-        ),
-        routes: {
-          AppRoutes.LOGIN: (_) => LoginScreen(),
-          AppRoutes.HOME: (_) => HomePage(),
-          AppRoutes.USER_LIST: (_) => UserList(),
-          AppRoutes.USER_FORM: (_) => UserForm(),
-        },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuController(),
+          ),
+        ],
+        child: MainScreen(),
       ),
     );
   }
